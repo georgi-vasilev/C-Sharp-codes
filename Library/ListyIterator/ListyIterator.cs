3b93df501d1f35af0128c74eefe15245cb9e75eb
Georgi Vasilev <35624206+georgi-vasilev@users.noreply.github.com>
2019-03-27 17:14:48 +0200

    Not a library.

---
+++ Library/ListyIterator/ListyIterator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+	class ListyIterator<T>:IEnumerable<T>
+	{
+		public List<T> elements { get; set; }
+		private int currentIndex;
+		private int nextIndex;
+		public ListyIterator(List<T> elements)
+		{
+			this.elements = elements;
+			this.currentIndex = 0;
+		}
+		public bool Move()
+		{
+			if(this.elements.Count-1>this.currentIndex)
+			{
+				this.currentIndex++;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public bool HasNext()
+		{
+			if(this.currentIndex==this.elements.Count-1)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		public void Print()
+		{
+			if(this.elements.Count==0)
+			{
+				Console.WriteLine("Invalid Operation!");
+			}
+			else
+			{
+				Console.WriteLine(this.elements[currentIndex]);
+			}
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (int i = 0; i < this.elements.Count; i++)
+			{
+				yield return this.elements[i];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+		public void PrintAll()
+		{
+			foreach (var element in elements)
+			{
+				Console.Write(element.ToString() + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Library/ListyIterator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> create = Console.ReadLine().Split(' ').ToList();
+			create.RemoveAt(0);
+			ListyIterator<string> input = new ListyIterator<string>(create);
+			string command = Console.ReadLine();
+			while (command!="END")
+			{
+				command = Console.ReadLine();
+				switch (command)
+				{
+					case "Move":
+						{
+							Console.WriteLine(input.Move());
+						}
+						break;
+					case "HasNext":
+						{
+							Console.WriteLine(input.HasNext());
+						}
+						break;
+					case "Print":
+						{
+							input.Print();
+						}
+						break;
+					case "PrintAll":
+						{
+							input.PrintAll();
+						}
+						break;
+				}
+			}
+		}
+	}
+}
